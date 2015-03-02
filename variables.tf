fb95fe9882e2fa2a9615da6bce30ff7ca0f177e0
Brandon Burton <brandon@inatree.org>
2015-03-02 21:08:09 +0000

    v0.1.0 - RDS module
        - Builds an RDS instance in a VPC
        - Builds an RDS Subnet Group
        - Can be Multi-AZ

---
+++ variables.tf
@@ -0,0 +1,55 @@
+//
+// Module: tf_aws_rds
+//
+
+// RDS Instance Variables
+
+variable "rds_instance_name" {}
+variable "rds_is_multi_az" {
+    default = "false"
+}
+
+variable "rds_allocated_storage" {
+    description = "The allocated storage in GBs"
+    // You just give it the number, e.g. 10
+}
+variable "rds_engine_type" {
+    // Valid types are
+    // - mysql
+    // - postgres
+    // - oracle-*
+    // - sqlserver-*
+    // http://docs.aws.amazon.com/AmazonRDS/latest/CommandLineReference/CLIReference-cmd-CreateDBInstance.html
+    // See --engine
+}
+
+variable "rds_engine_version" {
+    // For valid engine versions, see:
+    // See http://docs.aws.amazon.com/AmazonRDS/latest/CommandLineReference/CLIReference-cmd-CreateDBInstance.html
+    // --engine-version
+
+}
+
+variable "rds_instance_class" {}
+
+variable "database_name" {
+    description = "The name of the database to create"
+}
+
+variable "database_user" {}
+variable "database_password" {}
+
+variable "rds_security_group_id" {}
+
+variable "db_parameter_group" {
+    default = "default.mysql5.6"
+}
+
+// RDS Subnet Group Variables
+variable "subnet_az1" {}
+variable "subnet_az2" {}
+
+// Variables for providers used in this module
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
