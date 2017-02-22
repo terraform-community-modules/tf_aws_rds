fe1c089899f34718b4458d7596e2553ee600e168
Grzegorz Adamowicz <gadamowicz@gstlt.info>
2017-02-22 16:54:31 +0100

    Expand and make module more flexible.
    - removed provider - not desired in a module
    - changed rds_instance_name to more accurate rds_instance_identifier (to be consistent with Terraform nomenclature)
    - create own parameter group so we do not need to pass another ID to the module
    - get subnets in a list format for a case where we want to choose more than 2 subnets
    - create own security group and connect it to provided VPC
    - add description to variables

--- variables.tf
+++ variables.tf
@@ -1,63 +1,85 @@
-//
-// Module: tf_aws_rds
-//
+#
+# Module: tf_aws_rds
+#
 
-// RDS Instance Variables
+# RDS Instance Variables
 
-variable "rds_instance_name" {}
+variable "rds_instance_identifier" {
+    description = "Custom name of the instance"
+}
+
 variable "rds_is_multi_az" {
+    description = "Set to true on production"
     default = "false"
 }
 
 variable "rds_storage_type" {
+    description = "One of 'standard' (magnetic), 'gp2' (general purpose SSD), or 'io1' (provisioned IOPS SSD)."
     default = "standard"
 }
 
 variable "rds_allocated_storage" {
     description = "The allocated storage in GBs"
-    // You just give it the number, e.g. 10
+    # You just give it the number, e.g. 10
 }
+
 variable "rds_engine_type" {
-    // Valid types are
-    // - mysql
-    // - postgres
-    // - oracle-*
-    // - sqlserver-*
-    // See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
-    // --engine
+    # Valid types are
+    # - mysql
+    # - postgres
+    # - oracle-*
+    # - sqlserver-*
+    # See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+    # --engine
 }
 
 variable "rds_engine_version" {
-    // For valid engine versions, see:
-    // See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
-    // --engine-version
+    # For valid engine versions, see:
+    # See http://docs.aws.amazon.com/cli/latest/reference/rds/create-db-instance.html
+    # --engine-version
 
 }
 
-variable "rds_instance_class" {}
+variable "rds_instance_class" {
+    description = "Instance sizing"
+    # Valid values
+    # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+}
 
 variable "database_name" {
     description = "The name of the database to create"
 }
 
+# Sefl-explainatory variables
 variable "database_user" {}
 variable "database_password" {}
 
-variable "rds_security_group_id" {}
+variable "database_port" {}
 
+# This is for a custom parameter to be passed to the DB
+# We're "cloning" default ones, but we need to specify which should be copied
 variable "db_parameter_group" {
-    default = "default.mysql5.6"
+    # default = "mysql5.6"
+    default = "postgres9.5"
 }
 
 variable "publicly_accessible" {
+    description = "If database can be publicly available (NOT recommended)"
     default = "false"
 }
 
-// RDS Subnet Group Variables
-variable "subnet_az1" {}
-variable "subnet_az2" {}
+# RDS Subnet Group Variables
+variable "subnets" {
+    description = "List of subnets DB should be available at. It might be one subnet."
+    type = "list"
+}
 
-// Variables for providers used in this module
-variable "aws_access_key" {}
-variable "aws_secret_key" {}
-variable "aws_region" {}
+variable "private_cidr" {
+    description = "VPC private addressing, used for a security group"
+    type = "string"
+}
+
+variable "rds_vpc_id" {
+    description = "VPC to connect to, used for a security group"
+    type = "string"
+}
